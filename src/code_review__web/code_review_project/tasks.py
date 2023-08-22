c7b5cbbe1bba74f96c9762ebf8c68ec0ff2e5c2c
febux <deadcove@mail.ru>
2023-08-22 17:58:59 +0300

    Linters rules and configs were fixed.

--- src/code_review__web/code_review_project/tasks.py
+++ src/code_review__web/code_review_project/tasks.py
@@ -5,6 +5,6 @@ from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
 
-@shared_task
-def sample_task():
+@shared_task    # type: ignore
+def sample_task() -> None:
     logger.info("The sample task just ran.")
