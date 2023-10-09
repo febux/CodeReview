27e305678c13157ab34dd3dafde413cd40ce9c9b
febux <45841982+febux@users.noreply.github.com>
2023-10-09 21:04:49 +0300

    Update settings.py

--- src/code_review__web/code_review_project/settings.py
+++ src/code_review__web/code_review_project/settings.py
@@ -27,7 +27,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(distutils.util.strtobool(os.environ.get("DEBUG")))
+DEBUG = bool(distutils.util.strtobool(os.environ.get("DEBUG")))    # type: ignore
 
 if DEBUG:
     ALLOWED_HOSTS: List[str] = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0']
